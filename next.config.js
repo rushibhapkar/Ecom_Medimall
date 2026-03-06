209c3afe70a2e37db6e5fad09eea86b976d77c92
rushibhapkar <bhapkarrushikesh6@gmail.com>
2026-03-06 23:21:04 +0530

    Update Next.js config for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/Ecom_Medimall",
+  assetPrefix: "/Ecom_Medimall/",
 };
 
 module.exports = nextConfig;
